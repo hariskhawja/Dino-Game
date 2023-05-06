5da8f47a10896c7692e69d0ed4bda9581f974c8c
Haris Khawja <92524990+hariskhawja@users.noreply.github.com>
2023-05-06 10:28:34 -0400

    feat: complete

--- obstacles.py
+++ obstacles.py
@@ -13,20 +13,3 @@ class Obstacle:
     def obstacleDraw(self, screen):
         self.obstacleRect.center = (self.x, self.y)
         screen.blit(self.obstacle, self.obstacleRect)
-
-'''
-class Cactus:
-    def __init__(self, x, y, size):
-        self.x = x
-        self.y = y
-        self.cactus = pygame.image.load("cactus.png")
-        self.cactus = pygame.transform.scale(self.cactus, size)
-        self.cactusRect = self.cactus.get_rect()
-        self.cactusRect = self.cactusRect.inflate(-10, -10)
-        
-    def cactusDraw(self, screen):
-        self.cactusRect.center = (self.x, self.y)
-        screen.blit(self.cactus, self.cactusRect)
-
-class Pterodactyl'''
-
